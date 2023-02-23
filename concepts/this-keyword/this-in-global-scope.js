284269a4abbb3bc8131e14629c84f3fccce6280d
chandramohan.n <chandramohan.n@mamaearth.in>
2023-02-23 19:14:20 +0530

    javascript this keyword

---
+++ concepts/this-keyword/this-in-global-scope.js
@@ -0,0 +1,42 @@
+
+/**
+ * 
+ * 
+ * The use of this in the global scope
+ * In the global scope, when the code is executing in the browser, 
+ * all global variables and functions are defined on the window object. 
+ * Therefore, when we use this in a global function, 
+ * it refers to (and has the value of) the global window object 
+ * (not in strict mode though, as noted earlier) 
+ * that is the main container of the entire JavaScript application or web page.
+ * 
+ */
+
+var firstName = "Peter",
+    lastName = "Ally";
+
+function showFullName() {
+    // "this" inside this function will have the value of the window object
+    // because the showFullName () function is defined in the global scope, just like the firstName and lastName
+    console.log(this.firstName + " " + this.lastName);
+}
+
+var person = {
+    firstName: "Penelope",
+    lastName: "Barrymore",
+    showFullName: function () {
+        // "this" on the line below refers to the person object, because the showFullName function will be invoked by person object.
+        console.log(this.firstName + " " + this.lastName);
+    }
+}
+
+showFullName(); // Peter Ally
+
+// window is the object that all global variables and functions are defined on, hence:
+window.showFullName(); // Peter Ally
+
+// "this" inside the showFullName () method that is defined inside the person object still refers to the person object, hence:
+person.showFullName(); // Penelope Barrymore
+
+
+
---
+++ concepts/this-keyword/this-in-method-passes-as-callback.js
@@ -0,0 +1,74 @@
+
+
+
+
+// We have a simple object with a clickHandler method that we want to use when a button on the page is clicked
+var user = {
+    data: [
+        { name: "T. Woods", age: 37 },
+        { name: "P. Mickelson", age: 43 }
+    ],
+    clickHandler: function (event) {
+        var randomNum = ((Math.random() * 2 | 0) + 1) - 1; // random number between 0 and 1
+
+        // This line is printing a random person's name and age from the data array
+        console.log(this.data[randomNum].name + " " + this.data[randomNum].age);
+    }
+}
+
+// The button is wrapped inside a jQuery $ wrapper, so it is now a jQuery object
+// And the output will be undefined because there is no data property on the button object
+$("button").click(user.clickHandler); // Cannot read property '0' of undefined
+
+
+/****
+ * In the code above, since the button ($(“button”)) is an object on its own, 
+ * and we are passing the user.clickHandler method to its click() method as a callback,
+ *  we know that this inside our user.clickHandler method will no longer refer to 
+ * the user object. this will now refer to the object where the user.clickHandler
+ *  method is executed because this is defined inside the user.clickHandler method.
+ *  And the object that is invoking user.clickHandler is the button object—user.clickHandler
+ *  will be executed inside the button object’s click method.
+ * **/
+
+//! HERE button is invoking the callback, not user 
+
+/***
+ * 
+ * Note that even though we are calling the clickHandler () method with user.clickHandler 
+ * (which we have to do, since clickHandler is a method defined on user), 
+ * the clickHandler () method itself will be executed with the button object as the context 
+ * to which “this” now refers. So this now refers to is the button ($(“button”)) object.
+ * **/
+
+// !FIX
+// FIX
+// To fix this problem in the preceding example, we can use the bind method thus:
+
+// Instead of this line:
+
+//  $ ("button").click (user.clickHandler);
+// We have to bind the clickHandler method to the user object like this:
+// $("button").click (user.clickHandler.bind (user)); // P. Mickelson 43
+
+
+var user = {
+    data: [
+        { name: "T. Woods", age: 37 },
+        { name: "P. Mickelson", age: 43 }
+    ],
+    clickHandler: function (event) {
+        var randomNum = ((Math.random() * 2 | 0) + 1) - 1; // random number between 0 and 1
+
+        // This line is printing a random person's name and age from the data array
+        console.log(this.data[randomNum].name + " " + this.data[randomNum].age);
+    }
+
+}
+
+// Pass the user object's clickHandler method as a callback to the button's click method
+// The button is wrapped inside a jQuery $ wrapper, so it is now a jQuery object
+// And the output will be undefined
+$(".buttonError").click(user.clickHandler); // undefined
+
+$(".buttonGood").click(user.clickHandler.bind(user));
