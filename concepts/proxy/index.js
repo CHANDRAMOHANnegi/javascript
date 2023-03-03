484045904e5943ba594035964fce763e1322814b
cm <cmsingh1721997@gmail.com>
2023-03-03 20:42:45 +0000

    Update 2 files

---
+++ concepts/proxy/index.js
@@ -0,0 +1,23 @@
+const person = {
+  name: "chandra mohan",
+  nickName: "cm",
+  age: 26,
+  bornIn: "India",
+  occupation: "Software Engineer",
+};
+
+const personProxy = new Proxy(person, {
+  get: (obj, prop, receiver) => {
+    console.log(`${prop} is ${obj[prop]}`);
+    return prop in obj ? Reflect.get(obj, prop, receiver) : 420;
+  },
+  set: (obj, prop, value, receiver) => {
+    console.log(`${prop} changed from ${obj[prop]} to ${value}`);
+    prop[obj] = value;
+    Reflect.set(obj, prop, value, receiver);
+  },
+});
+
+personProxy["age"] = 24;
+// console.log(personProxy["age"]);
+// console.log(personProxy["abc"]);
