698f3fd36669c7260bcc88b6713865443db3c8f6
cm <cmsingh1721997@gmail.com>
2023-04-03 18:34:56 +0000

    adding oops

---
+++ closures/questions/2.js
@@ -0,0 +1,9 @@
+
+for (var i = 0; i < 3; i++) {
+
+    ((i)=> setTimeout(() => {
+       console.log(i)
+     }, 1000))(i)
+     
+   }
+   
---
+++ web-apis/set-timeout/set-timeout.js
@@ -0,0 +1,14 @@
+
+
+function myFunc(p1, p2) {
+    console.log("Parameters: " + p1 + " " + p2)
+}
+
+
+// setTimeout(function () {
+//     myFunc("param1", "param2")
+// }, 2000);
+
+
+
+setTimeout(myFunc, 2000, "param1", "param2");
