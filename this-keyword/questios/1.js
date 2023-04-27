563d7d785a47d6a692e96f0d17c4271719a07929
cm <cmsingh1721997@gmail.com>
2023-04-27 08:13:02 +0000

    added array sort , Map

---
+++ this-keyword/questios/1.js
@@ -0,0 +1,19 @@
+function Pet(name) {
+    this.name = name;
+
+    this.getName = () => this.name;
+}
+
+const cat = new Pet('Fluffy');
+
+console.log(cat.getName()); // What is logged?
+
+// this inside the arrow function equals to this of the outer scope
+
+const { getName } = cat;
+console.log(getName());     // What is logged?
+
+// output
+
+// Fluffy
+// Fluffy
