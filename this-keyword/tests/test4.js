c4ba54d98813222b67f482f55aa7ec9d3486f6d8
cm <cmsingh1721997@gmail.com>
2023-06-30 09:24:16 +0000

    promise polyfil

--- this-keyword/tests/test4.js
+++ this-keyword/tests/test4.js
@@ -1,15 +1,18 @@
 var length = 10;
 
 function fn() {
-    console.log(global.length, globalThis.length)
+    console.log(this.length, global.length, globalThis.length)
 }
+
+const arr = { "0": fn, "1": 1, "2": "cm", length: 14 }
 
 var obj = {
     length: 5,
     method: function (fn) {
         fn()
-        arguments[0]()
+        // console.log('---', arguments);
+        // arguments[0]()
     }
 }
 
-obj.method(fn, 1)
+obj.method(arr[0], 1)
