23c9561a0bcc865e75ec8059e204520da7bb15e2
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-01-18 00:17:15 +0530

    added memoized function

---
+++ vanilla-js-projects/budget-tracker/js/main.js
@@ -0,0 +1,26 @@
+var memoized = function (foo) {
+  var cache = {};
+  return function (...args) {
+    var argId = args.toString();
+    if (cache[argId]) {
+      return cache[argId];
+    } else {
+      var value = foo(...args);
+      cache[argId] = value;
+      return value;
+    }
+  };
+};
+
+var factor = function (n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+    console.log("in", n);
+    return n * factorial(n - 1);
+  }
+};
+
+var factorial = memoized(factor);
+console.log(factorial(5));
+console.log(factorial(6));
