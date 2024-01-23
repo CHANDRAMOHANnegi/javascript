e4c3fedccbc0d02636bb1c5ea048756551d892f0
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-01-23 13:38:47 +0530

    added render-props

---
+++ nextjs-client/app/design-pattern/render-props-pattern/input1.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export function Input1(props) {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Temp in °C"
+      />
+      {props.children(value)}
+    </>
+  );
+}
---
+++ nextjs-client/app/design-pattern/render-props-pattern/input2.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export function Input2(props) {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Temp in °C"
+      />
+      {props.render(value)}
+    </>
+  );
+}
