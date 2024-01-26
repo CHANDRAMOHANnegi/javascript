2876a1da9ba36d2379c4dac0e62bc0635f27327b
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-01-26 12:07:47 +0530

    adding class

---
+++ 8.oops/class/index.js
@@ -0,0 +1,43 @@
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+  sayHi() {
+    alert(this.name);
+  }
+}
+
+// class is a function
+alert(typeof User); // function
+
+// ...or, more precisely, the constructor method
+alert(User === User.prototype.constructor); // true
+
+// The methods are in User.prototype, e.g:
+alert(User.prototype.sayHi); // the code of the sayHi method
+
+// there are exactly two methods in the prototype
+alert(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
+
+/**
+ *
+ *  rewriting class User in pure functions
+ *  ==============================================================>>
+ *
+ * */
+
+// 1. Create constructor function
+function User(name) {
+  this.name = name;
+}
+// a function prototype has "constructor" property by default,
+// so we don't need to create it
+
+// 2. Add the method to prototype
+User.prototype.sayHi = function () {
+  alert(this.name);
+};
+
+// Usage:
+let user = new User("John");
+user.sayHi();
