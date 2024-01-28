f88c5779d2e27e2bab17e03d68e07b79e7bfdd85
Chandramohan negi <cnegi@zscaler.com>
2024-01-28 11:40:09 +0530

    added demo

--- 12.js-design-patterns/singleton/singleton.js
+++ 12.js-design-patterns/singleton/singleton.js
@@ -1,28 +1,27 @@
 var Singleton = (function () {
-    var instance;
-    function createInstance() {
-        var object = new Object("I am the instance");
-        return object;
-    }
+  var instance;
+  function createInstance() {
+    var object = new Object("I am the instance");
+    return object;
+  }
 
-    return {
-        getInstance: function () {
-            if (!instance) {
-                instance = createInstance();
-            }
-            return instance;
-        }
-    };
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
 })();
 
 function run() {
+  var instance1 = Singleton.getInstance();
+  console.log("instance1 : ", instance1);
+  var instance2 = Singleton.getInstance();
+  console.log("instance2 : ", instance2);
 
-    var instance1 = Singleton.getInstance();
-    console.log('instance1 : ', instance1);
-    var instance2 = Singleton.getInstance();
-    console.log('instance2 : ', instance2);
-
-    console.log("Same instance? " + (instance1 === instance2));
+  console.log("Same instance? " + (instance1 === instance2));
 }
 
-run()
+run();
