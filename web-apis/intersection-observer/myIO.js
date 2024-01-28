66c29c7d40297cd6b2c77a94ad378e47b27fe6af
Chandramohan negi <cnegi@zscaler.com>
2024-01-28 10:56:49 +0530

    refactor custom-intersection-observer

--- web-apis/intersection-observer/myIO.js
+++ web-apis/intersection-observer/myIO.js
@@ -1,48 +1,60 @@
+function calculateElementVisibility(element) {
+  const rect = element.getBoundingClientRect();
+
+  // Calculate the intersection height between the element and the viewport
+  const intersectionHeight =
+    Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0);
+
+  // Calculate the total height of the element
+  const elementHeight = Math.min(rect.height, window.innerHeight);
+
+  // Calculate the percentage of the element in the viewport
+  const percentageVisible = (intersectionHeight / elementHeight) * 100;
+
+  return percentageVisible;
+}
+
+function findIntersectionNodes(observers, broadcast) {
+  for (let i = 0; i < observers.length; i++) {
+    if (calculateElementVisibility(observers[i]) >= 0.5) {
+      observers[i].isIntersected = true;
+      broadcast();
+    } else {
+      observers[i].isIntersected = false;
+    }
+  }
+  return observers;
+}
+
 class IO {
-    constructor(cb){
-        this.observers = []
-        this.cb = cb;
-        window.addEventListener('scroll',()=>{
-            this.findIntersectionNodes();
-        })
-    }
-    subscribe(el){
-        this.observers.push(el)
-    }
-    calculateElementVisibility(element) {
-        const rect = element.getBoundingClientRect();
-        
-        // Calculate the intersection height between the element and the viewport
-        const intersectionHeight = Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0);
-        
-        // Calculate the total height of the element
-        const elementHeight = Math.min(rect.height, window.innerHeight);
-        
-        // Calculate the percentage of the element in the viewport
-        const percentageVisible = (intersectionHeight / elementHeight) * 100;
-        
-        return percentageVisible;
-    }
-    findIntersectionNodes(){
-         for (let i = 0; i < this.observers.length; i++) {
-           if(this.calculateElementVisibility(this.observers[i]) >= 0.5){
-            this.observers[i].isIntersected = true;
-            this.broadcast(); 
-           }
-            else
-            this.observers[i].isIntersected = false;
-         }
-    }
-    broadcast(){
-        this.cb(this.observers)
-    }
+  constructor(cb) {
+    this.observers = [];
+    this.cb = cb;
+    window.addEventListener("scroll", () => {
+      this.observers = findIntersectionNodes(
+        this.observers,
+        this.broadcast.bind(this)
+      );
+    });
+  }
+
+  subscribe(el) {
+    const idx = this.observers.length;
+    this.observers.push(el);
+    return () => {
+      this.observers.splice(idx, 1);
+    };
+  }
+
+  broadcast() {
+    this.cb(this.observers);
+  }
 }
 
 const boxElement = document.querySelector("#red");
 
 let observer = new IO((entries) => {
- entries.forEach((entry)=>console.log(entry.isIntersected,'test'));
+  entries.forEach((entry) => console.log(entry.isIntersected, "test"));
 });
 
-observer.subscribe(boxElement);
-
+const unSubscribe = observer.subscribe(boxElement);
