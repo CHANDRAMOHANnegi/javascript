ac446aaba92456ca6abf2d11ef0aca6421cc1460
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-02-01 16:35:22 +0530

    prototype question

--- 7.polyfill/string/repeat.js
+++ 7.polyfill/string/repeat.js
@@ -26,7 +26,23 @@ function myRepeat(repeat) {
   return "";
 }
 // console.log(str,'wtf'); // Reference error , temporal dead zone
-const str = "hello ";
-const res = myRepeat(2);
+// const str = "hello ";
+// const res = myRepeat(2);
 
-console.log(res);
+// console.log(res);
+
+if (!String.prototype.repeat) {
+  // if there's no such method
+  // add it to the prototype
+
+  String.prototype.repeat = function (n) {
+    // repeat the string n times
+
+    // actually, the code should be a little bit more complex than that
+    // (the full algorithm is in the specification)
+    // but even an imperfect polyfill is often considered good enough
+    return new Array(n + 1).join(this);
+  };
+}
+
+console.log("cm".repeat(5));
