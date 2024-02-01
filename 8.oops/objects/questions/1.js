e6370b6af97cf97f4542230ae02a03b64c838f9c
Chandramohan negi <cnegi@zscaler.com>
2024-02-01 16:35:22 +0530

    prototype question

---
+++ 8.oops/objects/questions/1.js
@@ -0,0 +1,6 @@
+let obj = {};
+
+console.log(obj.__proto__ === Object.prototype); // true
+
+console.log(obj.toString === obj.__proto__.toString); //true
+console.log(obj.toString === Object.prototype.toString); //true
