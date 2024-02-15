1b226a95dc6e45451155bafb2a4a4b9daf6b6f87
Chandramohan negi <cnegi@zscaler.com>
2024-02-15 14:23:26 +0530

    adding lld

--- nextjs-client/app/css/properties/page.tsx
+++ nextjs-client/app/css/properties/page.tsx
@@ -4,6 +4,7 @@ const links = [
   { link: "/", text: "hello" },
   { link: "/css/properties/margin", text: "margin" },
   { link: "/css/properties/position", text: "position" },
+  { link: "/css/flex", text: "flex" },
 ];
 
 export default function Home() {
