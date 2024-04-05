1c57f1af35717d8f46078d661ffc9c1722d1f14c
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-04-05 18:59:55 +0530

    adding output questions

---
+++ output-questions/3.js
@@ -0,0 +1,7 @@
+doit();
+var doit = function () {
+  alert("x");
+};
+
+// reference error, since doit will be undefined
+// hoisting
---
+++ output-questions/8.js
@@ -0,0 +1,3 @@
+console.log(4.toString()); //SyntaxError: Invalid or unexpected token
+console.log(4.2.toString()); // 4.2
+console.log(4*undefined); // NaN
