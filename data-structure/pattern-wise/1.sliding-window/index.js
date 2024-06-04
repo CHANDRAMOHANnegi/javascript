61b37b1c75b29d2ff54abd78235c290378aa8018
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-06-05 00:58:09 +0530

    dsa(sliding-window):[3] longest substring without repeating character https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- data-structure/pattern-wise/1.sliding-window/index.js
+++ data-structure/pattern-wise/1.sliding-window/index.js
@@ -1,5 +1,7 @@
 /***
  * 
+ * https://www.youtube.com/watch?v=-zSxTJkcdAo&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=3
+ * 
  * SLIDING WINDOW
  * 
  * we have to add element to window and REMOVE elements from window
