66881135bd1b8fde334db96c856212050eb70814
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-06-07 13:32:41 +0530

    dsa(sliding-window):[leetcode-1358] Number of Substrings Containing All Three Characters

---
+++ data-structure/pattern-wise/1.sliding-window/7.longest-k-uniq-substr.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @returns {number}
+ */
+
+class Solution {
+    longestKSubstr(s, k) {
+        const n = s.length
+
+        let left = 0
+        let right = 0
+        let maxLen = -1
+
+        let map = new Map()
+
+        while (right < n) {
+            /**
+             * I was making mistake here, I was setting index instead of count
+             * */
+            map.set(s[right], map.has(s[right]) ? map.get(s[right]) + 1 : 1)
+            while (map.size > k) {
+                const leftChar = s[left]
+                map.set(leftChar, map.get(leftChar) - 1)
+                if (map.get(leftChar) === 0) {
+                    map.delete(leftChar)
+                }
+                left++
+            }
+            /**
+             * I made mistake here, i dint use this check
+             * */
+            if (map.size === k) {
+                maxLen = Math.max(maxLen, right - left + 1)
+            }
+            right++
+        }
+
+        return maxLen
+    }
+}
+
+/****
+ * What went wrong
+ * 
+ * 1. I was blindly setting index as value of map, why why why
+ * i needed count when removing  so i need to set count
+ * 
+ * Focus Focus Focus Focus Focus Focus 
+ * 
+ * 2. I was updating max every time, I need to update max only when we have K unique elements
+ * 
+ * **/ 
