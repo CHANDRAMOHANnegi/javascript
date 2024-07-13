30902aef2eff8954c3e2361de93cc4a15a505219
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-07-13 23:29:01 +0530

    dsa refactor

--- data-structure/recursion/_all/1.all.js
+++ data-structure/recursion/_all/1.all.js
@@ -1,6 +1,6 @@
 function printS(idx, ds, total, target, arr, n) {
-    if(idx === n){
-        if(total === target){
+    if (idx === n) {
+        if (total === target) {
             console.log(ds);
         }
         return
@@ -13,12 +13,16 @@ function printS(idx, ds, total, target, arr, n) {
     total -= arr[idx]
     ds.pop()
 
+
+    /****
+     * 
+     * 
+     * **/
     printS(idx + 1, ds, total, target, arr, n)
 }
 
-
 function main() {
-    const arr = [1, 2, 3,4,5]
+    const arr = [1, 2, 3, 4, 5]
     const n = 5
     const target = 5
     const ds = []
