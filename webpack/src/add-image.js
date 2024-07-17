6ec246c582899826786cf4b10e4628c8e0f9fbb0
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-07-18 00:39:28 +0530

    chore(webpack):added webpack

---
+++ webpack/src/add-image.js
@@ -0,0 +1,14 @@
+import Kiwi from './kiwi.jpg'
+import altText from './altText.txt'
+
+function addImage() {
+    const img = document.createElement('img')
+    img.alt = altText
+    img.width = 300
+    img.src = Kiwi
+
+    const body = document.querySelector('body')
+    body.appendChild(img)
+}
+
+export default addImage
