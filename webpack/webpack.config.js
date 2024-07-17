3419fb2fe715d6b9979f7907cfbbb3ca75c9cd6b
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-07-18 00:39:28 +0530

    chore(webpack):added webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./dist"),
+        publicPath: "dist/"
+    },
+    mode: 'none',
+
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 3 * 1024 // 3kb
+                    }
+                }
+            },
+            {
+                test: /\.txt/,
+                type: "asset/source",
+            }
+        ]
+    }
+} 
