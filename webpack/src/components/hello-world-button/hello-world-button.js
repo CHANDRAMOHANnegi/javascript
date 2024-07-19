bdee7b40017486fcaa3773633f3ec3cea3549c87
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-07-19 20:22:11 +0530

    webpack: plugins, mode, css

--- webpack/src/components/hello-world-button/hello-world-button.js
+++ webpack/src/components/hello-world-button/hello-world-button.js
@@ -1,20 +1,19 @@
 import './hello-world-button.scss'
 
 class HelloWorldButton {
-
-    render(){
+    buttonCssClass = "hello-world-button"
+    render() {
         console.log('Hello world');
+        const body = document.querySelector("body")
         const button = document.createElement("button")
-        button.innerHTML = " Hello world"
-        button.classList.add('hello-world-button')
-
+        button.innerHTML = "Hello world"
         button.onclick = function () {
             const p = document.createElement("p")
             p.innerHTML = "Hello world"
             p.classList.add('hello-world-text')
             body.appendChild(p)
         }
-        const body = document.querySelector("body")
+        button.classList.add(this.buttonCssClass)
         body.appendChild(button)
     }
 }
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+// const HandleBarsLoader = require("handlebars-loader")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+  },
+  mode: "development",
+  devServer: {
+    port: 3001,
+    static: {
+      directory: path.resolve(__dirname, "./dist")
+    },
+    devMiddleware: {
+      index: 'index.html',
+      writeToDisk: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024, // 3kb
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      },{
+
+        test:/\.hbs$/,
+        use:['handlebars-loader']
+      }
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title:"Hello world",
+      template:"src/index.hbs",
+      description:"dev mode html",
+    })
+  ]
+};
