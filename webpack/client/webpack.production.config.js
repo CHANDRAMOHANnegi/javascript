1efb20d78883e1ebb1b502a85b88b83bb86a001f
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-07-20 21:58:56 +0530

    webpack: serve html file

--- webpack/client/webpack.production.config.js
+++ webpack/client/webpack.production.config.js
@@ -1,88 +1,64 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    "hello-world": "./src/kiwi.js",
-    kiwi: "./src/kiwi.js",
-  },
-
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "",
-  },
-  mode: "development",
-  devServer: {
-    port: 3001,
-    static: {
-      directory: path.resolve(__dirname, "./dist"),
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/static/'
     },
-    devMiddleware: {
-      index: "index.html",
-      writeToDisk: true,
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/env' ],
+                        plugins: [ '@babel/plugin-proposal-class-properties' ]
+                    }
+                }
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    'handlebars-loader'
+                ]
+            }
+        ]
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg)$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 3 * 1024, // 3kb
-          },
-        },
-      },
-      {
-        test: /\.txt/,
-        type: "asset/source",
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: "/node_modules/",
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/env"],
-            plugins: ["@babel/plugin-proposal-class-properties"],
-          },
-        },
-      },
-      {
-        test: /\.hbs$/,
-        use: ["handlebars-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: "hello-world.html",
-      chunks: ["hello-world"],
-      title: "Hello world",
-      template: "src/page-template.hbs",
-      description: "dev mode html hello",
-    }),
-    new HtmlWebpackPlugin({
-      filename: "kiwi.html",
-      title: "kiwi ",
-      chunks: ["kiwi"],
-      template: "src/page-template.hbs",
-      description: "dev mode html kiwi",
-    }),
-  ],
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.[contenthash].css'
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hello world',
+            description: 'Hello world',
+            template: 'src/page-template.hbs'
+        })
+    ]
 };
