47d82624a1d7e02bcedc866ac96563e6e6945d21
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-07-20 22:51:42 +0530

    webpack : module-federation

--- webpack/module-fed/kiwi/src/kiwi.js
+++ webpack/module-fed/kiwi/src/kiwi.js
@@ -1,8 +1,19 @@
-import Heading from './components/heading/heading.js';
-import KiwiImage from './components/kiwi-image/kiwi-image.js';
+import Heading from "./components/heading/heading.js";
+import KiwiImage from "./components/kiwi-image/kiwi-image.js";
 
 const heading = new Heading();
-heading.render('kiwi');
+heading.render("kiwi");
 const kiwiImage = new KiwiImage();
 kiwiImage.render();
 
+import("HelloWorldApp/HelloWorldButton")
+  .then((HelloWorldButtonModule) => {
+    const HelloWorldButton = HelloWorldButtonModule.default;
+
+    const button = new HelloWorldButton();
+
+    button.render();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
