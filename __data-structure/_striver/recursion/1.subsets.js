cd8787bfb5c94b90bd6c19f37518a3333b843b73
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-07-31 18:54:27 +0530

    chore(dsa): added recursion

---
+++ __data-structure/_striver/recursion/1.subsets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ 
+var subsets = function(nums) {
+    const res = []
+    sets(0,[],res,nums)
+    return res
+};
+
+var sets = function(idx,dp,res,nums){
+    if(idx === nums.length){
+        res.push([...dp])
+        return
+    }
+
+    dp.push(nums[idx])
+    sets(idx+1,dp,res,nums)
+    dp.pop()
+    sets(idx+1,dp,res,nums)
+}
