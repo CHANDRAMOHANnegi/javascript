9c10ac333d8d83618df6d397038c56992d752ccd
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-10-03 23:50:49 +0530

    dsa(blind-75): added array question

--- ___js/5.closures/questions/2.js
+++ ___js/5.closures/questions/2.js
@@ -1,9 +1,8 @@
 
 for (var i = 0; i < 3; i++) {
 
-    ((i)=> setTimeout(() => {
-       console.log(i)
-     }, 1000))(i)
-     
-   }
-   
+  ((i) => setTimeout(() => {
+    console.log(i)
+  }, 1000))(i)
+
+}
