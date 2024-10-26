b917d0831944a0f6b621f856fbe0759d5f2103a6
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-10-26 06:19:27 +0530

    dsa: add string to blind-75

---
+++ __data-structure/blind-75-leetcode/string/5.group-anagram.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    const map = new Map();
+
+    for (const str of strs) {
+        const sorted = str.split("").sort().join("");
+        if (map.has(sorted)) {
+            map.get(sorted).push(str);
+        } else {
+            map.set(sorted, [str]);
+        }
+    }
+
+    return Array.from(map.values());
+};
