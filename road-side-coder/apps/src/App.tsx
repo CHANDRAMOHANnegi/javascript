1b24ed27a5df93fb3c47d78e8fa3ce5eeb0782ca
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-11-06 14:18:33 +0530

    chore(road-side-coder): refactor app

---
+++ road-side-coder/apps/src/App.tsx
@@ -0,0 +1,12 @@
+import PollApp from './apps/polls/poll-app';
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-start pt-20 justify-center bg-gray-100">
+      <PollApp/>
+    </div>
+  );
+}
+
+export default App;
