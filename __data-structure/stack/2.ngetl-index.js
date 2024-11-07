b4b89896f1999902dfa5cafd97217c3e1a6829e9
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2024-11-07 14:34:47 +0530

    fix(dsa) : fix stack concepts

--- __data-structure/stack/2.ngetl-index.js
+++ __data-structure/stack/2.ngetl-index.js
@@ -1,10 +1,10 @@
-const ngetr = (arr = []) => {
+const ngetl = (arr = []) => {
     const stack = []
     const res = Array(arr.length).fill(-1);
 
-    let idx = arr.length - 1
+    let idx = 0
 
-    while (idx >= 0) {
+    while (idx < arr.length) {
         while (stack.length && arr[idx] >= arr[stack[stack.length - 1]]) {
             stack.pop()
         }
@@ -12,23 +12,16 @@ const ngetr = (arr = []) => {
             res[idx] = stack[stack.length - 1]
         }
         stack.push(idx)
-        idx--
+        idx++
     }
     return res
 };
 
-
 const arr = [2, 5, 9, 3, 1, 12, 6, 8, 7]
-console.log(ngetr(arr))
+console.log(ngetl(arr))
+
 
 /****
- * 
- * smaller will be pushed to the stack,
- * when greater appears it will, it will set itself as
- * answer to all smaller
- * 
- * stack me vo element honge jin se bada abhi tak mila nahi hai
- * 
  * 
  * O(2n)
  * inner loop run only n time
