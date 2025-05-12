a64be697bdd8bf285cb2f58e3e86b463afe08d0e
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2025-05-12 16:34:08 +0530

    chore(monorepo): add tailwind and storybook to vomponent library

--- monorepo/packages/ui/vite.config.ts
+++ monorepo/packages/ui/vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
-    conditions:  ["browser"]
+    conditions: ["browser"]
   },
-  build:{
-    lib:{
+  build: {
+    lib: {
       entry: resolve(__dirname, "./src/index.ts"),
       name: "@mono/ui",
       fileName: (format) => `index.${format}.js`,
