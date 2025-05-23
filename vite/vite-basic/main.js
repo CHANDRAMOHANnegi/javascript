d2b2a9ed54eba660039b9c1efc9a185e350dacb3
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2025-05-23 14:30:43 +0530

    chore(monorepo): added vite config

---
+++ vite/vite-basic/main.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+import file from './file';
+
+console.log(file);
+        import classes from "./style.module.css";
+        console.log(classes);
+        console.log(classes.title);
+
+        const title = document.querySelector(".title");
+        title.classList.add(classes.title);  
