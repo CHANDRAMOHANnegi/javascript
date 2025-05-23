9e537f3898c7d11f7ef92b921d357390e6226696
CHANDRAMOHANNEGI <cmsingh1721997@gmail.com>
2025-05-23 15:22:43 +0530

    chore(react-native): update react-native project to latest

---
+++ vite/vite-project/src/main.js
@@ -0,0 +1,27 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div class="bg-black m-4">
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1 class="text-6xl bg-black text-red-500">Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+console.log(import.meta.env);
+
+
+setupCounter(document.querySelector('#counter'))
